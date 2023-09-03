cc6a9a0563867d7da2188683c14968ca7e06d0cc
„christophbuehler“ <christoph@cbuehler.li>
2023-09-03 21:18:19 +0200

    another try.. I hate Window

--- RunDownload.bat
+++ RunDownload.bat
@@ -1,14 +1,11 @@
 @echo off
-
-:: Check for admin rights and elevate if not present
+:: Check for admin rights
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
     powershell -Command "Start-Process cmd -ArgumentList '/c cd %~dp0 && %0' -Verb RunAs"
     exit /b
 )
-pause
 
-:: Run your Python script
+:: Run the Python script
 python download.py
-
 pause
