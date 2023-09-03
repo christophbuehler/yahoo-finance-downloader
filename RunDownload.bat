ddc7917f0a032a1146c0f07324d7adb7d7b2eca7
„christophbuehler“ <christoph@cbuehler.li>
2023-09-03 21:10:05 +0200

    cwd

--- RunDownload.bat
+++ RunDownload.bat
@@ -3,11 +3,9 @@
 :: Check for admin rights and elevate if not present
 net session >nul 2>&1
 if %errorlevel% NEQ 0 (
-    powershell -Command "Start-Process -Verb RunAs -FilePath '%0'"
+    powershell -Command "Start-Process cmd -ArgumentList '/c cd %~dp0 && %0' -Verb RunAs"
     exit /b
 )
-
-pause
 
 :: Run your Python script
 python download.py
