aa755f067f23352e0f7ccd7448d9591d702e999f
„christophbuehler“ <christoph@cbuehler.li>
2023-09-03 21:39:31 +0200

    no comment

--- RunDownload.bat
+++ RunDownload.bat
@@ -1,23 +1,4 @@
-@echo off
-
-pause
-
-:: Check for admin rights
-echo Checking for admin rights...
-net session >nul 2>&1
-
-if %errorlevel% NEQ 0 (
-    echo Attempting to elevate...
-    :: powershell -Command "Start-Process cmd -ArgumentList '/c cd %~dp0 && %0' -Verb RunAs"
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~dp0\runDownload.bat' -Verb RunAs"
-
-    echo Exiting...
-    pause
-    exit /b
-) else (
-    echo Running with admin rights...
-    pause
-)
+echo Running download.py
 
 pause
 
