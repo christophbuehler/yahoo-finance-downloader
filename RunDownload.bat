46a0b8c240a323cfc1328ea1e7ebd7ce227d2624
„christophbuehler“ <christoph@cbuehler.li>
2023-09-03 21:32:05 +0200

    powershell

--- RunDownload.bat
+++ RunDownload.bat
@@ -8,7 +8,9 @@ net session >nul 2>&1
 
 if %errorlevel% NEQ 0 (
     echo Attempting to elevate...
-    powershell -Command "Start-Process cmd -ArgumentList '/c cd %~dp0 && %0' -Verb RunAs"
+    :: powershell -Command "Start-Process cmd -ArgumentList '/c cd %~dp0 && %0' -Verb RunAs"
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~dp0\runDownload.bat' -Verb RunAs"
+
     echo Exiting...
     pause
     exit /b
