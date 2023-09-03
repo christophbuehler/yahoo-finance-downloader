d34afa739b62e7d01985e1f782a1fcd0bbb6650c
„christophbuehler“ <christoph@cbuehler.li>
2023-09-03 20:57:03 +0200

    admin..

--- RunDownload.bat
+++ RunDownload.bat
@@ -1,1 +1,11 @@
+@echo off
+
+:: Check for admin rights and elevate if not present
+net session >nul 2>&1
+if %errorlevel% NEQ 0 (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0'"
+    exit /b
+)
+
+:: Run your Python script
 python download.py
